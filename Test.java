b66a5648b0567751ea7bee962b59118d74af22bb
shanukumar59720 <shanukumar59720>
2024-03-19 18:54:49 +0530

    array Practice code

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+
+    public static void main(String args[]){
+        int arr[] = new int [5];
+
+        System.out.print(arr);
+//    int[] a=new int[0];
+   
+//    System.out. println(a.length);
+   
+   }
+   
+   }
